7c7a5be4041b70df85f41c676a79518aebb8e929
j7ll7 <jelle.schaap@tatasteeleurope.com>
2025-06-06 09:45:42 +0200

    Remove unused imports

--- Alarm_algo.py
+++ Alarm_algo.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 """Utility script to merge trend and alarm data and mark failure periods."""
 
-import os
 import configparser
-from datetime import datetime
 import pandas as pd
 import pyodbc
 
